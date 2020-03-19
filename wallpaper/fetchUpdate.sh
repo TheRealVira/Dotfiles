81aed291c6a1d79f28fdcf6d44c33e73203ae9ba
TheRealVira <therealvira@gmail.com>
2020-03-19 12:57:38 +0100

    Fixed git issues and improved background script.

--- wallpaper/fetchUpdate.sh
+++ wallpaper/fetchUpdate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 WEBPAGE="https://apod.nasa.gov/apod/"
+WALLPAPER_DIR=~/.config/i3/wallpaper
 
 wget "${WEBPAGE}" --quiet -O temp.html
 
 grep -m 1 jpg "temp.html" | grep -o '".*"' | sed 's/"//g' | awk -v var="${WEBPAGE}" '{print var$1}' | xargs wget --quiet -O wallpaper.jpg
 
-rm ~/.config/i3/wallpaper/*
-cp wallpaper.jpg ~/.config/i3/wallpaper/
+rm -rf "${WALLPAPER_DIR}"
+mkdir "${WALLPAPER_DIR}"
+cp wallpaper.jpg "${WALLPAPER_DIR}"
