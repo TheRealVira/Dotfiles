65a393ea7f390885d9198038ba6f444480b365b2
TheRealVira <therealvira@gmail.com>
2020-03-21 21:08:27 +0100

    Enhanced wallpaper script to check whether updates are necessary.

--- wallpaper/fetchUpdate.sh
+++ wallpaper/fetchUpdate.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-WEBPAGE="https://apod.nasa.gov"
+WEBPAGE="https://apod.nasa.gov/apod"
 WALLPAPER_DIR=~/.config/i3/wallpaper
 mkdir -p "$WALLPAPER_DIR"
 
-curl -s "$WEBPAGE/apod/" | \
-	perl -ne 'print $1 if m|<a href="(image/[^"]+)"|' | \
-	wget -B "$WEBPAGE" -q -i - -O "$WALLPAPER_DIR/wallpaper.jpg"
+IMAGE=$(curl -s "$WEBPAGE/astropix.html" | perl -ne 'print $1 if m|<a href="(image/[^"]+)"|')
+wget -q -b "$WEBPAGE/$IMAGE" -O "$WALLPAPER_DIR/wallpaper-new.jpg"
+
+# Only replace file, if newer. (if checksum does not match)
+if [ $(! test -f "$WALLPAPER_DIR/wallpaper.jpg")] 
+then
+	if [$(md5sum "$WALLPAPER_DIR/wallpaper.jpg" -eq md5sum "$WALLPAPER_DIR/wallpaper-new.jpg")]
+	then
+		mv -f "$WALLPAPER_DIR/wallpaper-new.jpg" "$WALLPAPER_DIR/wallpaper.jpg"
+	else
+		rm "$WALLPAPER_DIR/wallpaper.jpg"
+	fi
+else
+	rm "$WALLPAPER_DIR/wallpaper-new.jpg"
+fi
