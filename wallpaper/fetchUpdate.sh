e70a38bdfa7faa29011a333c420853e0e08f3bbb
TheRealVira <therealvira@gmail.com>
2020-03-22 10:39:35 +0100

    Fixed script to detect when wget failed (e.g. when the picture of the day actually isn't a picture but a video.

--- wallpaper/fetchUpdate.sh
+++ wallpaper/fetchUpdate.sh
@@ -5,17 +5,13 @@ WALLPAPER_DIR=~/.config/i3/wallpaper
 mkdir -p "$WALLPAPER_DIR"
 
 IMAGE=$(curl -s "$WEBPAGE/astropix.html" | perl -ne 'print $1 if m|<a href="(image/[^"]+)"|')
-wget -q -b "$WEBPAGE/$IMAGE" -O "$WALLPAPER_DIR/wallpaper-new.jpg"
 
-# Only replace file, if newer. (if checksum does not match)
-if [ $(! test -f "$WALLPAPER_DIR/wallpaper.jpg")] 
+# Only replace file, if newer and wget succeeded. (if original file does not exist or the checksum does not match)
+if wget -b "$WEBPAGE/$IMAGE" -O "$WALLPAPER_DIR/wallpaper-new.jpg" || ! -f "$WALLPAPER_DIR/wallpaper.jpg" || md5sum "$WALLPAPER_DIR/wallpaper.jpg" -eq md5sum "$WALLPAPER_DIR/wallpaper-new.jpg"
 then
-	if [$(md5sum "$WALLPAPER_DIR/wallpaper.jpg" -eq md5sum "$WALLPAPER_DIR/wallpaper-new.jpg")]
-	then
-		mv -f "$WALLPAPER_DIR/wallpaper-new.jpg" "$WALLPAPER_DIR/wallpaper.jpg"
-	else
-		rm "$WALLPAPER_DIR/wallpaper.jpg"
-	fi
-else
 	mv -f "$WALLPAPER_DIR/wallpaper-new.jpg" "$WALLPAPER_DIR/wallpaper.jpg"
+	exit 1
+else
+	rm "$WALLPAPER_DIR/wallpaper-new.jpg"
+	exit 0
 fi
